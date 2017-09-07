a10addaf0841c191bf1f26eb2ef890fbf6f57b33
Thibault Vlacich <thibault.vlacich@gmail.com>
2017-09-07 16:14:09 +0200

    Rewrite of Browserify task to use glob

--- gulpfile.js/tasks/browserify.js
+++ gulpfile.js/tasks/browserify.js
@@ -1,20 +1,25 @@
 if(!PATH_CONFIG.javascripts) return
 
 const browserify = require('browserify')
+const buffer     = require('gulp-buffer')
 const gulp       = require('gulp')
 const path       = require('path')
 const source     = require('vinyl-source-stream')
+const tap        = require('gulp-tap')
 
 var browserifyTask = function() {
     var paths = {
-        src: path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.javascripts.src, 'main.js'),
+        src: path.resolve(process.env.PWD, PATH_CONFIG.src, PATH_CONFIG.javascripts.src),
         dest: path.resolve(process.env.PWD, PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest)
     }
 
-    return browserify(paths.src)
-        .bundle()
-        .pipe(source('main.js'))
-        .pipe(gulp.dest(paths.dest));
+    return gulp.src(paths.src, {read: false})
+        .pipe(tap(function (file) {
+            file.contents = browserify(file.path, {debug: true})
+                .bundle()
+        }))
+        .pipe(buffer())
+        .pipe(gulp.dest(paths.dest))
 }
 
 gulp.task('browserify', browserifyTask)
